56c274d33303bf53853efae3077d5e0139602dfa
GucioWons <wgutek1@wp.pl>
2025-02-13 17:42:06 +0100

    exercise-difficulty-and-equipment

---
+++ sherpout-server/src/main/java/com/sherpout/server/api/exercise/enumerated/ExerciseDifficulty.java
@@ -0,0 +1,7 @@
+package com.sherpout.server.api.exercise.enumerated;
+
+public enum ExerciseDifficulty {
+    EASY,
+    MEDIUM,
+    HARD
+}
---
+++ sherpout-server/src/main/java/com/sherpout/server/api/exercise/enumerated/ExerciseEquipment.java
@@ -0,0 +1,13 @@
+package com.sherpout.server.api.exercise.enumerated;
+
+public enum ExerciseEquipment {
+    BARBELL,
+    DUMBBELL,
+    KETTLEBELL,
+    RESISTANCE_BAND,
+    MACHINE,
+    CABLE_MACHINE,
+    PLATFORM,
+    BENCH,
+    PULL_UP_BAR,
+}

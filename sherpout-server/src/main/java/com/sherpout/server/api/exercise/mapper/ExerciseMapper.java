c22696fc1bc2c38b40607c953d0310a936e4cd4a
GucioWons <wgutek1@wp.pl>
2025-02-13 19:36:32 +0100

    added-exercise-mapper

---
+++ sherpout-server/src/main/java/com/sherpout/server/api/exercise/mapper/ExerciseMapper.java
@@ -0,0 +1,17 @@
+package com.sherpout.server.api.exercise.mapper;
+
+import com.sherpout.server.api.exercise.dto.ExerciseDTO;
+import com.sherpout.server.api.exercise.entity.Exercise;
+import com.sherpout.server.commons.mapper.ImageMapper;
+import com.sherpout.server.commons.mapper.TranslatedStringMapper;
+import org.mapstruct.Mapper;
+
+@Mapper(
+        componentModel = "spring",
+        uses = {
+                TranslatedStringMapper.class,
+                ImageMapper.class
+        })
+public interface ExerciseMapper {
+    ExerciseDTO mapToDTO(Exercise exercise);
+}

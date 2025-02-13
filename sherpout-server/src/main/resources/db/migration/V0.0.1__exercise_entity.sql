632f01cd471207b03dd9edaf3ce5bba59778c459
GucioWons <wgutek1@wp.pl>
2025-02-13 19:52:45 +0100

    corrected-migrations

--- sherpout-server/src/main/resources/db/migration/V0.0.1__exercise_entity.sql
+++ sherpout-server/src/main/resources/db/migration/V0.0.1__exercise_entity.sql
@@ -1,42 +1,43 @@
-CREATE TABLE public.image
+CREATE TABLE image
 (
-    id         bigserial NOT NULL,
-    image_data bytea     NOT NULL,
-    CONSTRAINT image_pk PRIMARY KEY (id)
+    id         bigserial PRIMARY KEY,
+    image_data bytea     NOT NULL
 );
 
-CREATE TABLE public.exercise
+CREATE TABLE exercise
 (
     name_en        varchar(128) NULL,
     name_pl        varchar(128) NULL,
     description_en varchar(1024) NULL,
     description_pl varchar(1024) NULL,
-    difficulty     int4 NULL,
-    target_muscle  int4 NULL,
-    id             uuid NOT NULL,
-    cover_id       uuid NULL,
-    CONSTRAINT exercise_pk PRIMARY KEY (id),
-    CONSTRAINT exercise_image_fk FOREIGN KEY (cover_id) REFERENCES public.image (id)
+    difficulty     int4 NOT NULL,
+    target_muscle  int4 NOT NULL,
+    id             bigserial PRIMARY KEY,
+    cover_id       bigint NULL,
+    CONSTRAINT exercise_image_fk FOREIGN KEY (cover_id) REFERENCES image (id) ON DELETE SET NULL
 );
 
-CREATE TABLE public.exercise_equipment
+CREATE TABLE exercise_equipment
 (
-    exercise_id  uuid NULL,
-    equipment_id int4 NULL,
-    CONSTRAINT exercise_equipment_exercise_fk FOREIGN KEY (exercise_id) REFERENCES public.exercise (id)
+    exercise_id  bigserial NOT NULL,
+    equipment_id int4 NOT NULL,
+    CONSTRAINT exercise_equipment_pk PRIMARY KEY (exercise_id, equipment_id),
+    CONSTRAINT exercise_equipment_exercise_fk FOREIGN KEY (exercise_id) REFERENCES exercise (id)
 );
 
-CREATE TABLE public.exercise_support_muscle
+CREATE TABLE exercise_support_muscle
 (
-    exercise_id uuid NULL,
-    muscle_id   int4 NULL,
-    CONSTRAINT exercise_support_muscle_exercise_fk FOREIGN KEY (exercise_id) REFERENCES public.exercise (id)
+    exercise_id bigserial NOT NULL,
+    muscle_id   int4 NOT NULL,
+    CONSTRAINT exercise_support_muscle_pk PRIMARY KEY (exercise_id, muscle_id),
+    CONSTRAINT exercise_support_muscle_exercise_fk FOREIGN KEY (exercise_id) REFERENCES exercise (id)
 );
 
-CREATE TABLE public.exercise_image
+CREATE TABLE exercise_image
 (
-    exercise_id uuid NULL,
-    image_id    uuid NULL,
-    CONSTRAINT exercise_image_exercise_fk FOREIGN KEY (exercise_id) REFERENCES public.exercise (id),
-    CONSTRAINT exercise_image_image_fk FOREIGN KEY (image_id) REFERENCES public.image (id)
+    exercise_id bigserial NOT NULL,
+    image_id    bigserial NOT NULL,
+    CONSTRAINT exercise_image_pk PRIMARY KEY (exercise_id, image_id),
+    CONSTRAINT exercise_image_exercise_fk FOREIGN KEY (exercise_id) REFERENCES exercise (id),
+    CONSTRAINT exercise_image_image_fk FOREIGN KEY (image_id) REFERENCES image (id)
 );

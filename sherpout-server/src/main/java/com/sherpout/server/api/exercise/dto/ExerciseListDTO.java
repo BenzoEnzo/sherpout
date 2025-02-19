16178cc0eb6db74b826c111c1abe7354ce366f8c
GucioWons <wgutek1@wp.pl>
2025-02-19 18:22:29 +0100

    created-exercise-list-dto

---
+++ sherpout-server/src/main/java/com/sherpout/server/api/exercise/dto/ExerciseListDTO.java
@@ -0,0 +1,23 @@
+package com.sherpout.server.api.exercise.dto;
+
+import com.sherpout.server.api.exercise.enumerated.ExerciseDifficulty;
+import com.sherpout.server.api.exercise.enumerated.Muscle;
+import com.sherpout.server.commons.dto.ImageDTO;
+import com.sherpout.server.commons.dto.TranslatedStringDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ExerciseListDTO {
+    private Long id;
+    private TranslatedStringDTO name;
+    private ExerciseDifficulty difficulty;
+    private Muscle targetMuscle;
+    private Set<Muscle> supportMuscles;
+    private ImageDTO cover;
+}

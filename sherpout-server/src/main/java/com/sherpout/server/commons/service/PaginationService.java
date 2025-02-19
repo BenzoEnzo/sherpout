6fe500244bf8829297ed8dc4a9801851d5ab378d
GucioWons <wgutek1@wp.pl>
2025-02-19 18:58:00 +0100

    pagination-service

---
+++ sherpout-server/src/main/java/com/sherpout/server/commons/service/PaginationService.java
@@ -0,0 +1,22 @@
+package com.sherpout.server.commons.service;
+
+import com.sherpout.server.commons.dto.PaginationDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Stream;
+
+@Service
+public class PaginationService {
+    public PageRequest getPageRequest(PaginationDTO pagination) {
+        int page = pagination.getPage() == null ? 0 : pagination.getPage();
+        int size = pagination.getSize() == null ? 10 : pagination.getSize();
+        return PageRequest.of(page, size);
+    }
+
+    public <T> Page<T> mapToPage(Stream<T> stream, Page<?> page) {
+        return new PageImpl<>(stream.toList(), page.getPageable(), page.getTotalElements());
+    }
+}

f45b45523f91e63b6057c05b2bd4603fe8a3ad38
GucioWons <wgutek1@wp.pl>
2025-02-20 20:51:54 +0100

    Corrected-jwt-handling

--- sherpout-server/src/main/java/com/sherpout/server/api/user/logic/TokenService.java
+++ sherpout-server/src/main/java/com/sherpout/server/api/user/logic/TokenService.java
@@ -1,21 +1,30 @@
 package com.sherpout.server.api.user.logic;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 import com.sherpout.server.api.user.dto.UserDto;
 import com.sherpout.server.config.security.group.UserGroup;
 
+import java.util.UUID;
+
 @Service
 public class TokenService {
-    public UserDto getUser(JwtAuthenticationToken auth) {
+    public UserDto getUser() {
+        JwtAuthenticationToken auth = getCurrentAuthentication();
         UserDto user = new UserDto();
+        user.setId(UUID.fromString(getClaim(auth, "uuid")));
         user.setGroup(getGroup(auth));
         user.setFirstName(getClaim(auth, "given_name"));
         user.setLastName(getClaim(auth, "family_name"));
         user.setEmail(getClaim(auth, "email"));
         user.setEmailVerified(getClaim(auth, "email_verified"));
         return user;
+    }
+
+    private JwtAuthenticationToken getCurrentAuthentication() {
+        return (JwtAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
     }
 
     private UserGroup getGroup(JwtAuthenticationToken auth) {

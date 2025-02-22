14820a5458de7928301aa65c4108b1c9410fc52f
DELL <goxer@o2.pl>
2025-02-22 06:15:10 +0100

    record entity and table init

---
+++ sherpout-server/src/main/java/com/sherpout/server/api/record/entity/Record.java
@@ -0,0 +1,23 @@
+package com.sherpout.server.api.record.entity;
+
+import com.sherpout.server.api.exercise.entity.Exercise;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+public class Record {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private ZonedDateTime date;
+    private Integer value;
+    private UUID userId;
+    @ManyToOne
+    private Exercise exercise;
+}
---
+++ sherpout-server/src/main/resources/db/migration/V0.0.2__record_entity.sql
@@ -0,0 +1,8 @@
+CREATE TABLE record (
+id BIGSERIAL PRIMARY KEY,
+date TIMESTAMP WITH TIME ZONE,
+value INTEGER,
+user_id UUID,
+exercise_id BIGINT,
+CONSTRAINT fk_exercise FOREIGN KEY (exercise_id) REFERENCES exercise(id)
+);

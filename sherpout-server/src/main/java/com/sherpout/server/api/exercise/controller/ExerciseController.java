b8ed912490da2da5bfabf6310bf1d2cf059e5d3a
GucioWons <wgutek1@wp.pl>
2025-03-06 21:54:38 +0100

    resolved-conflicts

--- sherpout-server/src/main/java/com/sherpout/server/api/exercise/controller/ExerciseController.java
+++ sherpout-server/src/main/java/com/sherpout/server/api/exercise/controller/ExerciseController.java
@@ -2,13 +2,11 @@ package com.sherpout.server.api.exercise.controller;
 
 import com.sherpout.server.api.exercise.dto.ExerciseDTO;
 import com.sherpout.server.api.exercise.dto.ExerciseListDTO;
-import com.sherpout.server.api.exercise.enumerated.MuscleCategory;
 import com.sherpout.server.api.exercise.dto.LikeNumberResponseDTO;
+import com.sherpout.server.api.exercise.enumerated.MuscleCategory;
 import com.sherpout.server.api.exercise.logic.ExerciseLikeService;
 import com.sherpout.server.api.exercise.logic.ExerciseService;
 import com.sherpout.server.api.user.logic.TokenService;
-import com.sherpout.server.commons.dto.pagination.PageResponseDTO;
-import com.sherpout.server.commons.dto.pagination.PaginationDTO;
 import com.sherpout.server.config.security.group.SecuredByGroup;
 import com.sherpout.server.config.security.group.UserGroup;
 import jakarta.validation.Valid;

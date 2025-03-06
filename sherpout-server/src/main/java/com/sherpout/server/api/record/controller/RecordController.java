9ac5bfb35e88cfba6fe4388f8f4d203303b8c778
GucioWons <wgutek1@wp.pl>
2025-03-06 22:03:13 +0100

    added-endpoint

--- sherpout-server/src/main/java/com/sherpout/server/api/record/controller/RecordController.java
+++ sherpout-server/src/main/java/com/sherpout/server/api/record/controller/RecordController.java
@@ -8,10 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("records")
@@ -25,5 +22,13 @@ public class RecordController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(recordService.createRecord(request));
+    }
+
+    @DeleteMapping("{id}")
+    @SecuredByGroup(UserGroup.USER)
+    public ResponseEntity<?> deleteRecord(@PathVariable Long id) {
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .body(null);
     }
 }

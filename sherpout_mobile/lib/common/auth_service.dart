7c2e3305afbe9bf3194338e26891ce5933b569ea
GucioWons <wgutek1@wp.pl>
2025-03-07 23:37:38 +0100

    auth-service

---
+++ sherpout_mobile/lib/common/auth_service.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_appauth/flutter_appauth.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class AuthService {
+  final FlutterAppAuth _appAuth = FlutterAppAuth();
+  final FlutterSecureStorage _secureStorage = FlutterSecureStorage();
+
+  final String clientId = dotenv.env['KEYCLOAK_CLIENT_ID']!;
+  final String redirectUrl = dotenv.env['KEYCLOAK_REDIRECT_URL']!;
+  final String issuer = dotenv.env['KEYCLOAK_URL']!;
+
+  Future<void> login(BuildContext context) async {
+    try {
+      final AuthorizationTokenResponse result =
+          await _appAuth.authorizeAndExchangeCode(
+        AuthorizationTokenRequest(clientId, redirectUrl,
+            issuer: issuer,
+            scopes: ['openid', 'profile', 'email'],
+            allowInsecureConnections: true),
+      );
+
+      await _secureStorage.write(key: 'access_token', value: result.accessToken);
+      await _secureStorage.write(key: 'id_token', value: result.idToken);
+      if (context.mounted) {
+        Navigator.pushReplacementNamed(context, '/dashboard');
+      }
+    } catch (e) {
+      print("Błąd logowania: $e");
+    }
+  }
+}

739a4c7bf5101041cc81e6684a15039caf0e33f1
GucioWons <wgutek1@wp.pl>
2025-03-07 20:57:39 +0100

    Working-login-page

--- sherpout_mobile/lib/pages/dashboard/dashboard_page.dart
+++ sherpout_mobile/lib/pages/dashboard/dashboard_page.dart
@@ -1,9 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'package:sherpoutmobile/navigation/menu/left_side_menu.dart';
 import 'package:sherpoutmobile/navigation/menu/right_side_menu.dart';
+
+final FlutterAppAuth appAuth = FlutterAppAuth();
+final FlutterSecureStorage secureStorage = FlutterSecureStorage();
+
+const String clientId = "mobile-app";
+const String redirectUrl = "com.sherpout.sherpoutmobile:/oauth2redirect";
+const String issuer = "http://192.168.100.45:8082/realms/Sherpout/";
+
+Future<void> login() async {
+  try {
+    final AuthorizationTokenResponse? result =
+        await appAuth.authorizeAndExchangeCode(
+      AuthorizationTokenRequest(clientId, redirectUrl,
+          issuer: issuer,
+          scopes: ['openid', 'profile', 'email'],
+          allowInsecureConnections: true),
+    );
+
+    if (result != null) {
+      await secureStorage.write(key: 'access_token', value: result.accessToken);
+      await secureStorage.write(key: 'id_token', value: result.idToken);
+    }
+  } catch (e) {
+    print("Błąd logowania: $e");
+  }
+}
 
 class DashboardPage extends StatelessWidget {
   final String userName;

f550e6f61bfd5c78e576f14fb4f0718a8c283b33
GucioWons <wgutek1@wp.pl>
2025-03-09 14:17:37 +0100

    injected-auth-service-in-the-auth-page

--- sherpout_mobile/lib/pages/auth/auth_page.dart
+++ sherpout_mobile/lib/pages/auth/auth_page.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:get_it/get_it.dart';
 import 'package:sherpoutmobile/common/auth_service.dart';
 
-final FlutterAppAuth appAuth = FlutterAppAuth();
-final FlutterSecureStorage secureStorage = FlutterSecureStorage();
+final getIt = GetIt.instance;
 
 class AuthPage extends StatelessWidget {
-  const AuthPage({super.key});
+  final AuthService _authService = GetIt.instance<AuthService>();
+
+  AuthPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final authService = AuthService();
 
     return Scaffold(
       body: Center(
@@ -21,7 +20,7 @@ class AuthPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: () => authService.login(context),
+              onPressed: () => _authService.login(context),
               child: Text(AppLocalizations.of(context)!.loginOrRegister),
             ),
           ],

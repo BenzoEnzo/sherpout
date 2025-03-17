b310075135eecbd76637ba63b150757694a39b99
GucioWons <wgutek1@wp.pl>
2025-03-17 21:27:43 +0100

    changes-in-the-login-method

--- sherpout_mobile/lib/common/api_client.dart
+++ sherpout_mobile/lib/common/api_client.dart
@@ -1,23 +1,37 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:sherpoutmobile/common/auth_service.dart';
 
 class ApiClient {
   final Dio _dio;
   final FlutterSecureStorage _secureStorage;
+  final AuthService _authService;
 
-  ApiClient(this._dio, this._secureStorage) {
+  ApiClient(this._dio, this._secureStorage, this._authService) {
     _dio.options.baseUrl = dotenv.env['API_BASE_URL']!;
     _dio.interceptors.add(InterceptorsWrapper(
       onRequest: (options, handler) async {
-        final token = await _secureStorage.read(key: 'access_token');
-        if (token != null) {
-          options.headers["Authorization"] = "Bearer $token";
-        }
+        await _addAuthHeader(options);
         options.headers["Content-Type"] = "application/json";
         return handler.next(options);
       },
+      onError: (DioException e, handler) async {
+        if (e.response?.statusCode == 401) {
+          await _authService.refreshToken();
+          await _addAuthHeader(e.requestOptions);
+          return handler.resolve(await _dio.fetch(e.requestOptions));
+        }
+        return handler.next(e);
+      }
     ));
+  }
+
+  Future<void> _addAuthHeader(RequestOptions options) async {
+    final token = await _secureStorage.read(key: 'access_token');
+    if (token != null) {
+      options.headers["Authorization"] = "Bearer $token";
+    }
   }
 
   Future<Response> get(String endpoint,
--- sherpout_mobile/lib/common/auth_service.dart
+++ sherpout_mobile/lib/common/auth_service.dart
@@ -3,8 +3,6 @@ import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get_it/get_it.dart';
-import 'package:provider/provider.dart';
-import 'package:sherpoutmobile/common/user_provider.dart';
 
 final getIt = GetIt.instance;
 
@@ -19,54 +17,39 @@ class AuthService {
   AuthService(this._appAuth, this._secureStorage);
 
   Future<void> login(BuildContext context) async {
-    try {
-      final AuthorizationTokenResponse result =
-          await _appAuth.authorizeAndExchangeCode(
-        AuthorizationTokenRequest(clientId, redirectUrl,
-            issuer: issuer,
-            scopes: ['openid', 'profile', 'email'],
-            allowInsecureConnections: true),
-      );
+    final AuthorizationTokenResponse result =
+        await _appAuth.authorizeAndExchangeCode(
+      AuthorizationTokenRequest(clientId, redirectUrl,
+          issuer: issuer,
+          scopes: ['openid', 'profile', 'email'],
+          allowInsecureConnections: true),
+    );
 
-      if (!context.mounted) return;
-      await _saveTokensAndFetchUser(context, result);
-
-      if (!context.mounted) return;
-      Navigator.pushReplacementNamed(context, '/dashboard');
-    } catch (e) {
-      print("Błąd logowania: $e");
-    }
+    await _saveTokens(result);
   }
 
-  Future<void> refreshToken(BuildContext context) async {
-    try {
-      final TokenResponse result = await _appAuth.token(
-        TokenRequest(clientId, redirectUrl,
-            discoveryUrl: '$issuer/protocol/openid-connect/token',
-            refreshToken: await _secureStorage.read(key: 'refresh_token'),
-            scopes: ['openid', 'profile', 'email'],
-            allowInsecureConnections: true),
-      );
+  Future<void> refreshToken() async {
+    final TokenResponse result = await _appAuth.token(
+      TokenRequest(clientId, redirectUrl,
+          discoveryUrl: '$issuer/protocol/openid-connect/token',
+          refreshToken: await _secureStorage.read(key: 'refresh_token'),
+          scopes: ['openid', 'profile', 'email'],
+          allowInsecureConnections: true),
+    );
 
-      if (!context.mounted) return;
-      _saveTokensAndFetchUser(context, result);
-    } catch (e) {
-      print("Błąd odświeżania tokenu: $e");
-    }
+    _saveTokens(result);
   }
 
-  Future<void> _saveTokensAndFetchUser(BuildContext context, TokenResponse result) async {
+  Future<void> _saveTokens(TokenResponse result) async {
     await Future.wait([
       _secureStorage.write(key: 'access_token', value: result.accessToken),
       _secureStorage.write(key: 'id_token', value: result.idToken),
       _secureStorage.write(key: 'refresh_token', value: result.refreshToken),
     ]);
-
-    if (!context.mounted) return;
-    await Provider.of<UserProvider>(context, listen: false).fetch();
   }
 
   Future<bool> isUserLoggedIn() async {
     final accessToken = await _secureStorage.read(key: 'access_token');
-    return accessToken != null;  }
+    return accessToken != null;
+  }
 }

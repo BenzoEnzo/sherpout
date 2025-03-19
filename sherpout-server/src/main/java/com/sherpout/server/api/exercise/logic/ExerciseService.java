1846d4d16533e3f42048126df20b91ce6bb6d666
Gustaw Wąsowski <96374672+GucioWons@users.noreply.github.com>
2025-03-19 19:34:31 +0100

    S/GYMUP-59/filter-exercises (#20)
    * Created-query-param
    
    * Grouping-exercises-by-muscle-category
    
    * resolved-conflicts
    
    * merge-corrections

--- sherpout-server/src/main/java/com/sherpout/server/api/exercise/logic/ExerciseService.java
+++ sherpout-server/src/main/java/com/sherpout/server/api/exercise/logic/ExerciseService.java
@@ -3,28 +3,27 @@ package com.sherpout.server.api.exercise.logic;
 import com.sherpout.server.api.exercise.dto.ExerciseDTO;
 import com.sherpout.server.api.exercise.dto.ExerciseListDTO;
 import com.sherpout.server.api.exercise.entity.Exercise;
+import com.sherpout.server.api.exercise.enumerated.MuscleCategory;
 import com.sherpout.server.api.exercise.mapper.ExerciseMapper;
 import com.sherpout.server.api.exercise.repository.ExerciseRepository;
-import com.sherpout.server.commons.dto.pagination.PageResponseDTO;
-import com.sherpout.server.commons.dto.pagination.PaginationDTO;
-import com.sherpout.server.commons.service.PaginationService;
 import com.sherpout.server.error.exception.SingleApiErrorException;
 import com.sherpout.server.error.model.ApiError;
 import com.sherpout.server.error.model.ErrorLocationType;
 import com.sherpout.server.error.model.ErrorMessage;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ExerciseService {
     private final ExerciseRepository exerciseRepository;
     private final ExerciseMapper exerciseMapper;
-    private final PaginationService paginationService;
 
     @Transactional
     public ExerciseDTO updateExercise(ExerciseDTO updateRequest, Long id) {
@@ -52,9 +51,11 @@ public class ExerciseService {
                 .orElseThrow(IllegalArgumentException::new);
     }
 
-    public PageResponseDTO<ExerciseListDTO> getAllExercises(PaginationDTO pagination) {
-        PageRequest pageRequest = paginationService.getPageRequest(pagination);
-        Page<Exercise> exercisesPage = exerciseRepository.findAll(pageRequest);
-        return paginationService.mapToPage(exercisesPage.stream().map(exerciseMapper::mapToListDTO), exercisesPage);
+    public Map<MuscleCategory, List<ExerciseListDTO>> getAllExercises() {
+        return exerciseRepository.findAll().stream()
+                .collect(Collectors.groupingBy(
+                        exercise -> exercise.getTargetMuscle().getCategory(),
+                        Collectors.mapping(exerciseMapper::mapToListDTO, Collectors.toList())
+                ));
     }
 }

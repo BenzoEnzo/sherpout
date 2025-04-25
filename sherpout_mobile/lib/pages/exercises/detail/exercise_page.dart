54dca7a236d0a22511ef015fbef31416be1d5d6f
GucioWons <wgutek1@wp.pl>
2025-04-25 22:50:40 +0200

    getting-exercise-from-api

--- sherpout_mobile/lib/pages/exercises/detail/exercise_page.dart
+++ sherpout_mobile/lib/pages/exercises/detail/exercise_page.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:sherpoutmobile/common/dto/exercise_dto.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../../../common/components/loading_component.dart';
+import '../../../services/exercise_service.dart';
 
 class ExercisePage extends StatefulWidget {
   const ExercisePage({super.key});
@@ -8,10 +15,51 @@ class ExercisePage extends StatefulWidget {
 }
 
 class _ExercisePageState extends State<ExercisePage> {
+  final ExerciseService _exerciseService = GetIt.instance<ExerciseService>();
+
+  ExerciseDto? _exercise;
+  bool _isLoading = true;
+  String? _error;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadExercise();
+  }
+
+  Future<void> _loadExercise() async {
+    try {
+      final String idParam = ModalRoute.of(context)?.settings.name?.split('/').last ?? '';
+      final exerciseId = int.tryParse(idParam) ?? 0;
+
+      final exercise = await _exerciseService.getById(exerciseId);
+
+      setState(() {
+        _exercise = exercise;
+        _isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        _error = 'Błąd ładowania ćwiczenia: $e';
+        _isLoading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(AppLocalizations.of(context)!.exercises),
+        ),
+        body: LoadingComponent(
+          isLoading: _isLoading,
+          error: _error,
+          child: Center(
+            child: Text(_exercise!.name.localized(context)),
+          )
+        )
+    );
   }
   
 }

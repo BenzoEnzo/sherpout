a7d12eb0650ac9ee2683596e24e4579541e4a103
DELL <goxer@o2.pl>
2025-04-26 22:23:41 +0200

    during

--- sherpout_mobile/lib/pages/exercises/list/exercise_list_item.dart
+++ sherpout_mobile/lib/pages/exercises/list/exercise_list_item.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:provider/provider.dart';
 import 'package:sherpoutmobile/common/dto/exercise_list_dto.dart';
 
+import '../../../services/image_service.dart';
 import 'exercise_cover.dart';
 import 'exercise_summary.dart';
 
@@ -16,7 +19,10 @@ class ExerciseListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        ExerciseCover(difficulty: exercise.difficulty),
+        ExerciseCover(
+            difficulty: exercise.difficulty,
+            objectName: "6%2F520f6f96-5266-4890-8f5c-226b5cceed48.jpg",
+        ),
         SizedBox(width: 16),
         Expanded(
           child: ExerciseSummary(

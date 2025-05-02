092f26fa1555f39354ae7004252c5e51523dae90
DELL <goxer@o2.pl>
2025-05-02 19:05:27 +0200

    fix fe pr

--- sherpout_mobile/lib/pages/records/list/records_page.dart
+++ sherpout_mobile/lib/pages/records/list/records_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:get_it/get_it.dart';
 import 'package:sherpoutmobile/common/components/loading_component.dart';
 import 'package:sherpoutmobile/common/dto/record_dto.dart';
-import 'package:sherpoutmobile/pages/records/list/record_item.dart';
+import 'package:sherpoutmobile/pages/records/list/record_list_item.dart';
 import 'package:sherpoutmobile/services/record_service.dart';
 
 import 'new_record_button.dart';

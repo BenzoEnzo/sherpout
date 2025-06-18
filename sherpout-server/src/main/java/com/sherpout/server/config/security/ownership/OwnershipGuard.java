e270c73d4aaf464c3f1d6f7777f13414843a0b63
GucioWons <wgutek1@wp.pl>
2025-06-18 23:06:37 +0200

    Admin-permission

--- sherpout-server/src/main/java/com/sherpout/server/config/security/ownership/OwnershipGuard.java
+++ sherpout-server/src/main/java/com/sherpout/server/config/security/ownership/OwnershipGuard.java
@@ -1,11 +1,11 @@
 package com.sherpout.server.config.security.ownership;
 
+import com.sherpout.server.api.user.dto.UserDto;
 import com.sherpout.server.api.user.logic.TokenService;
+import com.sherpout.server.config.security.group.UserGroup;
 import com.sherpout.server.error.exception.AccessForbiddenException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -13,8 +13,8 @@ public class OwnershipGuard {
     private final TokenService tokenService;
 
     public void checkOwnership(Ownable ownable) {
-        UUID currentUserId = tokenService.getUserId();
-        if (!ownable.getUserId().equals(currentUserId)) {
+        UserDto user = tokenService.getUser();
+        if (!ownable.getUserId().equals(user.getId()) && !user.getGroup().equals(UserGroup.ADMIN)) {
             throw new AccessForbiddenException();
         }
     }

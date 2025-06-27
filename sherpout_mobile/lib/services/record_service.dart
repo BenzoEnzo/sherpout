4367fff15c972fa26fe0eb9249f98a5e308c3f97
DELL <goxer@o2.pl>
2025-06-27 19:56:23 +0200

    added record history service to flutter component

--- sherpout_mobile/lib/services/record_service.dart
+++ sherpout_mobile/lib/services/record_service.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:dio/dio.dart';
+import 'package:sherpoutmobile/common/dto/date_range_query_param.dart';
 
 import '../common/api_client.dart';
 import '../common/dto/record_dto.dart';
@@ -10,6 +13,12 @@ class RecordService {
 
   Future<List<RecordDTO>> getRecords() async {
     Response<dynamic> response = await _apiClient.get('records');
+    final List<dynamic> data = response.data;
+    return data.map((json) => RecordDTO.fromJson(json)).toList();
+  }
+
+  Future<List<RecordDTO>> getRecordHistory(Long exerciseId, DateRangeQueryParam dateRangeQueryParam) async {
+    Response<dynamic> response = await _apiClient.get('records/history/$exerciseId', queryParameters: dateRangeQueryParam.asQueryParameters());
     final List<dynamic> data = response.data;
     return data.map((json) => RecordDTO.fromJson(json)).toList();
   }

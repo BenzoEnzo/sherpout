09b443d2224aad4eb8abf4400e7725003ad2c492
DELL <goxer@o2.pl>
2025-06-27 21:49:58 +0200

    update record_list_item

--- sherpout_mobile/lib/pages/records/list/record_list_item.dart
+++ sherpout_mobile/lib/pages/records/list/record_list_item.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:sherpoutmobile/common/dto/record_dto.dart';
-import 'package:sherpoutmobile/pages/records/list/record_list_item_footer.dart';
-import 'package:sherpoutmobile/pages/records/list/record_list_item_header.dart';
-import '../../../common/theme/app_colors.dart';
+
+import '../../../../common/dto/record_dto.dart';
+import '../../../../common/theme/app_colors.dart';
 import '../../exercises/list/exercise_cover.dart';
+import '../detail/record_detail_page.dart';
+import 'record_list_item_footer.dart';
+import 'record_list_item_header.dart';
 
 class RecordItem extends StatelessWidget {
   final RecordDTO record;
@@ -18,33 +20,40 @@ class RecordItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final formattedDate = DateFormat('dd.MM.yyyy').format(record.date!);
 
-    return Container(
-      padding: const EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        border: Border.all(color: AppColors.secondary, width: 1),
+    return InkWell(
+      onTap: () => Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (_) => RecordDetailPage(record: record),
+        ),
       ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          ExerciseCover(cover: record.exercise!.cover),
-          const SizedBox(width: 16),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                RecordListItemHeader(exerciseName: record.exercise!.name),
-                RecordListItemFooter(formattedDate: formattedDate, weight: record.value!),
-              ],
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(color: AppColors.secondary, width: 1),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ExerciseCover(cover: record.exercise!.cover),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  RecordListItemHeader(exerciseName: record.exercise!.name),
+                  RecordListItemFooter(
+                    formattedDate: formattedDate,
+                    weight: record.value!,
+                  ),
+                ],
+              ),
             ),
-          ),
-          const Icon(Icons.chevron_right),
-        ],
+            const Icon(Icons.chevron_right),
+          ],
+        ),
       ),
     );
   }
 }
-
-
-
-

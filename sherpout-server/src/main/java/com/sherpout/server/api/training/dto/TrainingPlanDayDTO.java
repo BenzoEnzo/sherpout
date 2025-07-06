f6be04614e6888e1c85bb4b766748e7e9caca3a7
GucioWons <wgutek1@wp.pl>
2025-07-06 14:53:37 +0200

    dtos

---
+++ sherpout-server/src/main/java/com/sherpout/server/api/training/dto/TrainingPlanDayDTO.java
@@ -0,0 +1,27 @@
+package com.sherpout.server.api.training.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TrainingPlanDayDTO {
+    @NotNull
+    @Min(1)
+    @Max(7)
+    private Integer number;
+
+    @Valid
+    @NotNull
+    @NotEmpty
+    @Size(min = 1, max = 16)
+    private List<TrainingPlanExerciseDTO> exercises;
+}
---
+++ sherpout-server/src/main/java/com/sherpout/server/api/training/dto/TrainingPlanExerciseDTO.java
@@ -0,0 +1,26 @@
+package com.sherpout.server.api.training.dto;
+
+import com.sherpout.server.api.exercise.dto.ExerciseSelectDTO;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TrainingPlanExerciseDTO {
+    @Valid
+    @NotNull
+    private ExerciseSelectDTO exercise;
+
+    @NotNull
+    @Min(1)
+    @Max(16)
+    private Integer sets;
+}

0984f580d6dcbf690236256ff48cbbc08352da53
GucioWons <wgutek1@wp.pl>
2025-07-08 20:57:45 +0200

    Added-after-mappings

--- sherpout-server/src/main/java/com/sherpout/server/api/training/mapper/TrainingPlanDayMapper.java
+++ sherpout-server/src/main/java/com/sherpout/server/api/training/mapper/TrainingPlanDayMapper.java
@@ -1,31 +1,57 @@
 package com.sherpout.server.api.training.mapper;
 
 import com.sherpout.server.api.training.dto.TrainingPlanDayDTO;
+import com.sherpout.server.api.training.dto.TrainingPlanExerciseDTO;
 import com.sherpout.server.api.training.entity.TrainingPlanDay;
 import com.sherpout.server.api.training.entity.TrainingPlanExercise;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Mapper(
         componentModel = "spring",
         uses = TrainingPlanExerciseMapper.class
 )
-public interface TrainingPlanDayMapper {
-    TrainingPlanDayDTO mapToDTO(TrainingPlanDay trainingPlanDay);
+public abstract class TrainingPlanDayMapper {
+    @Autowired
+    private TrainingPlanExerciseMapper trainingPlanExerciseMapper;
 
-    @Mapping(target = "id", ignore = true)
-    TrainingPlanDay mapToEntity(TrainingPlanDayDTO dto);
+    public abstract TrainingPlanDayDTO mapToDTO(TrainingPlanDay trainingPlanDay);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "exercises", ignore = true)
-    TrainingPlanDay mapToUpdateEntity(TrainingPlanDayDTO dto, @MappingTarget TrainingPlanDay trainingPlanDay);
+    public abstract TrainingPlanDay mapToEntity(TrainingPlanDayDTO dto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "exercises", ignore = true)
+    public abstract TrainingPlanDay mapToUpdateEntity(TrainingPlanDayDTO dto, @MappingTarget TrainingPlanDay trainingPlanDay);
 
     @AfterMapping
-    default void setBackReference(@MappingTarget TrainingPlanDay day) {
-        for (TrainingPlanExercise exercise : day.getExercises()) {
-            exercise.setTrainingPlanDay(day);
-        }
+    protected void mapDays(TrainingPlanDayDTO dto, @MappingTarget TrainingPlanDay trainingPlanDay) {
+        updateTrainingPlanExercises(trainingPlanDay.getExercises(), dto.getExercises(), trainingPlanDay);
+    }
+
+    public void updateTrainingPlanExercises(List<TrainingPlanExercise> entities, List<TrainingPlanExerciseDTO> dtos, TrainingPlanDay day) {
+        Map<Long, TrainingPlanExercise> entitiesMap = entities.stream()
+                .collect(Collectors.toMap(TrainingPlanExercise::getId, exercise -> exercise));
+
+        entities.clear();
+
+        entities.addAll(dtos.stream()
+                .map(dayDto -> createOrUpdateExercise(entitiesMap.get(dayDto.getId()), dayDto))
+                .peek(exercise -> exercise.setTrainingPlanDay(day))
+                .toList());
+    }
+
+    private TrainingPlanExercise createOrUpdateExercise(TrainingPlanExercise entity, TrainingPlanExerciseDTO dto) {
+        return entity == null
+                ? trainingPlanExerciseMapper.mapToEntity(dto)
+                : trainingPlanExerciseMapper.mapToUpdateEntity(dto, entity);
     }
 }
--- sherpout-server/src/main/java/com/sherpout/server/api/training/mapper/TrainingPlanMapper.java
+++ sherpout-server/src/main/java/com/sherpout/server/api/training/mapper/TrainingPlanMapper.java
@@ -1,31 +1,53 @@
 package com.sherpout.server.api.training.mapper;
 
 import com.sherpout.server.api.training.dto.TrainingPlanDTO;
+import com.sherpout.server.api.training.dto.TrainingPlanDayDTO;
 import com.sherpout.server.api.training.entity.TrainingPlan;
 import com.sherpout.server.api.training.entity.TrainingPlanDay;
-import org.mapstruct.AfterMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Mapper(
         componentModel = "spring",
         uses = TrainingPlanDayMapper.class
 )
-public interface TrainingPlanMapper {
-    TrainingPlanDTO mapToDTO(TrainingPlan trainingPlan);
+public abstract class TrainingPlanMapper {
+    @Autowired
+    protected TrainingPlanDayMapper trainingPlanDayMapper;
+
+    public abstract TrainingPlanDTO mapToDTO(TrainingPlan trainingPlan);
 
     @Mapping(target = "id", ignore = true)
-    TrainingPlan mapToEntity(TrainingPlanDTO dto);
+    public abstract TrainingPlan mapToEntity(TrainingPlanDTO dto);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "days", ignore = true)
-    TrainingPlan mapToUpdateEntity(TrainingPlanDTO dto, @MappingTarget TrainingPlan trainingPlan);
+    public abstract TrainingPlan mapToUpdateEntity(TrainingPlanDTO dto, @MappingTarget TrainingPlan trainingPlan);
 
     @AfterMapping
-    default void setBackReference(@MappingTarget TrainingPlan plan) {
-        for (TrainingPlanDay day : plan.getDays()) {
-            day.setTrainingPlan(plan);
-        }
+    protected void mapDays(TrainingPlanDTO dto, @MappingTarget TrainingPlan trainingPlan) {
+        updateTrainingPlanDays(trainingPlan.getDays(), dto.getDays(), trainingPlan);
+    }
+
+    public void updateTrainingPlanDays(List<TrainingPlanDay> entities, List<TrainingPlanDayDTO> dtos, TrainingPlan trainingPlan) {
+        Map<Long, TrainingPlanDay> entitiesMap = entities.stream()
+                .collect(Collectors.toMap(TrainingPlanDay::getId, day -> day));
+
+        entities.clear();
+
+        entities.addAll(dtos.stream()
+                .map(dayDto -> createOrUpdateDay(entitiesMap.get(dayDto.getId()), dayDto))
+                .peek(day -> day.setTrainingPlan(trainingPlan))
+                .toList());
+    }
+
+    private TrainingPlanDay createOrUpdateDay(TrainingPlanDay entity, TrainingPlanDayDTO dto) {
+        return entity == null
+                ? trainingPlanDayMapper.mapToEntity(dto)
+                : trainingPlanDayMapper.mapToUpdateEntity(dto, entity);
     }
 }
